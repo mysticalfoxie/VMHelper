bc643f1a454ad9fdfb06912a853535870671983a
Michelle-99 <MichelleBecker2003@gmail.com>
2021-08-22 15:46:51 +0200

    Add silent start

--- Program.cs
+++ Program.cs
@@ -1,21 +1,80 @@
-using System;
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using AudioProcessHelper.Services;
 using AudioProcessHelper.UI;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.Data.Xml.Dom;
+using Windows.System;
+using Windows.UI.Notifications;
 
 namespace AudioProcessHelper
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main(string[] launchArguments)
         {
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            if (launchArguments.Select(x => x.ToLower(CultureInfo.InvariantCulture)).Contains("--silent"))
+            {
+                JustGo();
+            }
+            else
+            {
+                Application.Run(new MainForm());
+            }
+        }
+
+        private static void JustGo()
+        {
+            try
+            {
+                string[] processes = GetProcesses();
+                ManipulateProcesses(processes);
+                new ToastContentBuilder()
+                    .AddText("Successfully manipulated the process(es):")
+                    .AddText(string.Join(", ", processes.Select(x => $"\"{x}.exe\"").ToArray()))
+                    .SetToastDuration(ToastDuration.Short)
+                    .Show();
+            }
+            catch (Exception ex)
+            {
+                new ToastContentBuilder()
+                    .AddText("An error occured!")
+                    .AddText($"{ex.Message}")
+                    .Show();
+            }
+            finally
+            {
+                Application.Exit();
+            }
+        }
+
+        private static void ManipulateProcesses(string[] names)
+        {
+            Process[] processes = ProcessService.GetProcesses(names);
+            ProcessService.IncreasePriority(processes);
+            ProcessService.SetAffinity(processes);
+        }
+
+        private static string[] GetProcesses()
+        {
+            return ConfigService
+                .Load()
+                .Processes
+                .Where(x => x.Checked)
+                .Select(x => x.Name)
+                .ToArray();
         }
     }
 }
